0461d5a16b39eeb40f4d4f0a2915fdba03d10734
nischaldutt <nischaldutt01@gmail.com>
2022-06-03 01:22:02 +0530

    feat: adding deleteBlog feature

--- react/state-management-context-usereducer/src/components/Blog.js
+++ react/state-management-context-usereducer/src/components/Blog.js
@@ -2,13 +2,17 @@ import React, { useContext } from "react";
 import { BlogContext } from "../context/BlogContext";
 
 const Blog = () => {
-  const { state: blogs, addBlog } = useContext(BlogContext);
+  const { state: blogs, addBlog, deleteBlog } = useContext(BlogContext);
 
   return (
     <div>
       <button onClick={addBlog}>Add</button>
       {blogs.map((blog) => {
-        return <div key={blog.title}>{blog.title}</div>;
+        return (
+          <div onClick={() => deleteBlog(blog.id)} key={blog.id}>
+            {blog.title}
+          </div>
+        );
       })}
     </div>
   );
--- react/state-management-context-usereducer/src/context/BlogContext.js
+++ react/state-management-context-usereducer/src/context/BlogContext.js
@@ -3,7 +3,15 @@ import createDataContext from "./createDataContext";
 const blogReducer = (state, action) => {
   switch (action.type) {
     case "add":
-      return [...state, { title: `Blog #${state.length + 1}` }];
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 9999),
+          title: `Blog #${state.length + 1}`,
+        },
+      ];
+    case "delete":
+      return state.filter((blog) => blog.id !== action.payload);
     default:
       return state;
   }
@@ -15,5 +23,11 @@ const addBlog = (dispatch) => {
   };
 };
 
+const deleteBlog = (dispatch) => {
+  return (id) => {
+    dispatch({ type: "delete", payload: id });
+  };
+};
+
 export const { Context: BlogContext, Provider: BlogProvider } =
-  createDataContext(blogReducer, { addBlog }, []);
+  createDataContext(blogReducer, { addBlog, deleteBlog }, []);
