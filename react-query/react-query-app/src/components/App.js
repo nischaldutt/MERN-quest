59b562c0ea555c1cd0ae74ff1bee8ca996ef7ddb
nischaldutt <nischaldutt01@gmail.com>
2022-06-03 22:46:06 +0530

    feat: adding boilerplate along with react-query connection

---
+++ react-query/react-query-app/src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>hello world!</div>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
+  );
+};
+
+export default App;
