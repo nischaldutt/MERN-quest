7df33fd050aa85d317a560a498bbe547b7f9b1fc
nischaldutt <nischaldutt01@gmail.com>
2022-06-07 22:29:20 +0530

    docs: addding alternate fetching options for getStaticProps

--- nextjs/nextjs-blog/pages/index.js
+++ nextjs/nextjs-blog/pages/index.js
@@ -3,6 +3,19 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 
+// export async function getSortedPostsData() {
+//   // Instead of the file system,
+//   // fetch post data from an external API endpoint
+//   const res = await fetch("..");
+//   return res.json();
+// }
+
+// export async function getSortedPostsData() {
+//   // Instead of the file system,
+//   // fetch post data from a database
+//   return databaseClient.query('SELECT posts...')
+// }
+
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
