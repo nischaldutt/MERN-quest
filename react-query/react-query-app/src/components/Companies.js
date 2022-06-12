efae6897d699351546f4df008893a195a62f58a3
nischaldutt <nischaldutt01@gmail.com>
2022-06-12 13:53:12 +0530

    blog:  setting staleTime

--- react-query/react-query-app/src/components/Companies.js
+++ react-query/react-query-app/src/components/Companies.js
@@ -13,16 +13,21 @@ const Companies = () => {
     data: companies,
     isError,
     error,
-  } = useQuery("companies", fetchCompanies);
+    isFetching,
+    refetch,
+  } = useQuery("companies", fetchCompanies, {
+    cacheTime: 20000,
+    staleTime: 10000,
+  });
 
-  console.log({ res: useQuery("companies", fetchCompanies) });
+  console.log({ isLoading, isFetching });
 
   if (isLoading) {
-    return <div>loading</div>;
+    return <h1>loading</h1>;
   }
 
   if (isError) {
-    return <div>{error.message}</div>;
+    return <h1>{error.message}</h1>;
   }
 
   return (
@@ -30,8 +35,67 @@ const Companies = () => {
       {companies.map((company) => {
         return <h1 key={company.id}>{company.name}</h1>;
       })}
+      <button onClick={refetch}>refetch</button>
     </div>
   );
 };
 
 export default Companies;
+
+// import React, { useEffect } from "react";
+
+// async function fetchCompanies() {
+//   return await fetch(`http://localhost:4000/companies`).then((response) =>
+//     response.json()
+//   );
+// }
+
+// const Companies = () => {
+//   const [companies, setCompanies] = React.useState([]);
+//   const [isLoading, setIsLoading] = React.useState(true);
+//   const [isError, setIsError] = React.useState(false);
+
+//   useEffect(() => {
+//     fetchCompanies()
+//       .then((response) => {
+//         setCompanies(response);
+//         setIsLoading(false);
+//       })
+//       .catch((error) => {
+//         setIsError(true);
+//         setIsLoading(false);
+//       });
+//   }, []);
+
+//   if (isLoading) {
+//     return <h1>loading</h1>;
+//   }
+
+//   if (isError) {
+//     return <h1>error occurred</h1>;
+//   }
+
+//   const refetch = () => {
+//     setIsLoading(true);
+//     fetchCompanies()
+//       .then((response) => {
+//         setCompanies(response);
+//         setIsLoading(false);
+//       })
+//       .catch((error) => {
+//         setIsError(true);
+//         setIsLoading(false);
+//       });
+//   };
+
+//   return (
+//     <div>
+//       {companies.map((company) => {
+//         return <h1 key={company.id}>{company.name}</h1>;
+//       })}
+//       <button onClick={refetch}>refetch</button>
+//     </div>
+//   );
+// };
+
+// export default Companies;
