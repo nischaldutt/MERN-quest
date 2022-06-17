6f81c905f8e6e4a57bba2e7f960549b098603e6b
nischaldutt <nischaldutt01@gmail.com>
2022-06-17 22:47:10 +0530

    blog: avoiding loading state by setting initialQueryData config

--- react-query/react-query-app/src/components/company.js
+++ react-query/react-query-app/src/components/company.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { useQuery, useQueries } from "react-query";
+import { useQuery, useQueries, useQueryClient } from "react-query";
 
 const fetchCompany = ({ queryKey }) => {
   const id = queryKey[1];
@@ -9,40 +9,59 @@ const fetchCompany = ({ queryKey }) => {
   );
 };
 
-const componyIds = [1, 2, 3];
+const fetchCompanyDetails = ({ queryKey }) => {
+  const id = queryKey[1];
+  return fetch(`http://localhost:4000/companyDetails/${id}`).then((response) =>
+    response.json()
+  );
+};
+
+// const componyIds = [1, 2, 3];
 
 const Company = () => {
   const { id } = useParams();
-  // const {
-  //   isLoading,
-  //   data: companyData,
-  //   isError,
-  //   error,
-  // } = useQuery(["company", id], fetchCompany);
+  const queryClient = useQueryClient();
 
-  const response = useQueries(
-    componyIds.map((id) => {
-      return {
-        queryKey: ["company", id],
-        queryFn: fetchCompany,
-      };
-    })
-  );
+  const {
+    isLoading,
+    data: companyData,
+    isError,
+    error,
+  } = useQuery(["company-details", id], fetchCompanyDetails, {
+    initialData: () => {
+      const cachedCompanyData = queryClient
+        .getQueryData("companies")
+        .find((cachedCompany) => cachedCompany.id === parseInt(id));
 
-  console.log({ response });
+      console.log({ cachedCompanyData });
+      return cachedCompanyData ? cachedCompanyData : undefined;
+    },
+  });
 
-  // if (isLoading) {
-  //   return <h1>loading</h1>;
-  // }
+  // const response = useQueries(
+  //   componyIds.map((id) => {
+  //     return {
+  //       queryKey: ["company", id],
+  //       queryFn: fetchCompany,
+  //     };
+  //   })
+  // );
 
-  // if (isError) {
-  //   return <h1>{error.message}</h1>;
-  // }
+  // console.log({ response });
+
+  if (isLoading) {
+    return <h1>loading</h1>;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
 
   return (
     <div>
-      {/* <h2>{companyData.name}</h2>
-      <h2>{companyData.ceo}</h2> */}
+      <h2>name: {companyData.name}</h2>
+      <h2>ceo: {companyData.ceo}</h2>
+      <h2>founder: {companyData.founder}</h2>
     </div>
   );
 };
