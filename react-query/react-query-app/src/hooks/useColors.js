0087901f37633bf72e4e8138fc4dfee3de951344
nischaldutt <nischaldutt01@gmail.com>
2022-06-21 22:26:41 +0530

    blog: inifinite queries

--- react-query/react-query-app/src/hooks/useColors.js
+++ react-query/react-query-app/src/hooks/useColors.js
@@ -2,13 +2,17 @@ import { useInfiniteQuery } from "react-query";
 import axios from "axios";
 import { request } from "../utils/requestInterceptor";
 
-const fetchColors = ({ pageParam = 1 }) => {
+const fetchColors = async (props) => {
+  console.log({ props });
+  // return fetch(`/colors?_limit=2&_page=${1}`).then((response) =>
+  //   response.json()
+  // );
   // return axios.get(`http://localhost:4000/colors?_limit=2&_page=${pageParam}`);
-  return request({ url: `/colors?_limit=2&_page=${pageParam}`, method: "get" });
+  return request({ url: `/colors?_limit=2&_page=${1}`, method: "get" });
 };
 
 const useColors = () => {
-  return useInfiniteQuery(["colors"], fetchColors, {
+  return useInfiniteQuery(["games"], fetchColors, {
     getNextPageParam: (_lastPage, pages) => {
       return pages.length < 5 ? pages.length + 1 : undefined;
     },
