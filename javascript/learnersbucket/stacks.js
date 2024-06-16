3030bc004309861197a7e28e50e7fc72bcdd9fb8
nischal <nischaldutt01@gmail.com>
2024-06-16 17:29:54 +0530

    stacks in js

---
+++ javascript/learnersbucket/stacks.js
@@ -0,0 +1,25 @@
+class Stack {
+  constructor() {
+    this.arr = [];
+  }
+
+  push(n) {
+    this.arr.push(n);
+  }
+
+  pop() {
+    return this.arr.pop();
+  }
+
+  top() {
+    return this.arr[this.arr.length - 1];
+  }
+
+  isEmpty() {
+    return this.arr.length === 0;
+  }
+
+  size() {
+    return this.arr.length;
+  }
+}
