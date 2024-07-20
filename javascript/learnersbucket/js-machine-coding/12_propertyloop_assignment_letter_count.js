a5d50423dee6892930c55faf408d98dfb197e67b
nischal <nischaldutt01@gmail.com>
2024-07-21 00:14:07 +0530

    propertyloo[ assessment letter count

---
+++ javascript/learnersbucket/js-machine-coding/12_propertyloop_assignment_letter_count.js
@@ -0,0 +1,31 @@
+// problem: https://github.com/ratracegrad/coderbyte-Beginner/blob/master/Letter%20Count%20I
+
+function LetterCount(str) {
+  let result = -1;
+  const obj = {
+    repeatedChars: 0,
+    s: "",
+  };
+
+  const words = str.split(" ");
+  for (let word of words) {
+    let freq = {};
+    let count = 0;
+    for (let c of word) {
+      if (freq[c] === undefined) freq[c] = 1;
+      else {
+        if (freq[c] === 1) count++;
+        freq[c] = freq[c] + 1;
+      }
+    }
+
+    if (Object.keys(freq).length < word.length) {
+      if (obj.repeatedChars < count) {
+        obj.repeatedChars = count;
+        obj.s = word;
+      }
+    }
+  }
+
+  return obj.repeatedChars === 0 ? -1 : obj.s;
+}
