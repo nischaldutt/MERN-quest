84b01f6880bfb9e4f46ed4209111b8b6249fa7ee
nischal <nischaldutt01@gmail.com>
2024-07-28 17:56:54 +0530

    circuuit breaker in js

---
+++ javascript/learnersbucket/js-machine-coding/15_circuit_breaker.js
@@ -0,0 +1,53 @@
+function myFunction() {
+  let count = 0;
+  return function () {
+    count++;
+    if (count < 4) throw "error";
+    else console.log("success");
+  };
+}
+
+function circuitBreaker(callback, failCount, timeThreshold) {
+  let fails = 0;
+  let timeSinceLastFailure = 0;
+  let flag = true;
+
+  return function (...args) {
+    if (flag === false) {
+      if (Date.now() - timeSinceLastFailure <= timeThreshold) {
+        console.log("unavailable");
+        return;
+      } else {
+        fails = 0;
+        timeSinceLastFailure = 0;
+        flag = true;
+      }
+    }
+
+    try {
+      const res = callback(...args);
+      fails = 0;
+      timeSinceLastFailure = 0;
+      return res;
+    } catch (e) {
+      fails++;
+      timeSinceLastFailure = Date.now();
+      if (fails >= failCount) flag = false;
+      console.log("error");
+    }
+  };
+}
+
+const test = circuitBreaker(myFunction(), 3, 1000);
+test();
+test();
+test();
+
+test();
+test();
+test();
+test();
+test();
+test();
+
+setTimeout(() => test(), 1000);
