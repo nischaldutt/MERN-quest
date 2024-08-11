816861f1ee9f845c5e5b56e67b55cfef584dbc0a
nischal <nischaldutt01@gmail.com>
2024-08-11 12:58:34 +0530

    currying in js

--- javascript/learnersbucket/js-machine-coding/24_currying_4.js
+++ javascript/learnersbucket/js-machine-coding/24_currying_4.js
@@ -1,39 +1,22 @@
-// function add(...args1) {
-//   let sum = 0;
-//   for (let n of args1) sum += n;
-//   return function (...args2) {
-//     add(...args2, sum);
-//   };
-// }
-
-// console.log(add(1)(2).value() == 3);
-// console.log(add(1, 2)(3).value() == 6);
-// console.log(add(1)(2)(3).value() == 6);
-// console.log(add(1)(2) + 3);
-
-// function curry(callback) {
-//   let sum = 0;
-//   return function (...args) {
-//     sum = callback(...args);
-//     return sum;
-//   };
-// }
-
-// function sum(a, b, c, d) {
-//   return a + b + c + d;
-// }
-
-// let curriedSum = curry(sum);
-
-// console.log(curriedSum(1, 2, 3, 4, 5));
-// console.log(curriedSum(1)(2, 3)(4, 5));
-// console.log(curriedSum(1)(2)(3)(4));
-
-function sum(a) {
-  return function (b) {
-    if (b === undefined) return a;
-    return sum(a + b);
-  };
+function curry(callback) {
+  function helper(...args) {
+    if (args.length >= callback.length) {
+      return callback(...args);
+    } else {
+      function helper2(...args2) {
+        return helper(...args, ...args2);
+      }
+      return helper2;
+    }
+  }
+  return helper;
 }
 
-console.log(sum(1)(2)(3)(4)());
+function sum(a, b, c, d) {
+  return a + b + c + d;
+}
+
+let curriedSum = curry(sum);
+console.log(curriedSum(1, 2, 3, 4, 5));
+console.log(curriedSum(1)(2, 3)(4, 5));
+console.log(curriedSum(1)(2)(3)(4));
