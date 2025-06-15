61c5b46357b290e972ae660b7c9c6be0ca1107e5
nischal dutt <nischaldutt01@gmail.com>
2025-06-15 17:58:19 +0530

    leetcode combination sum variation with permutations

---
+++ stoneycodes70/27_combination_sum_0.js
@@ -0,0 +1,25 @@
+var combinationSum = function (candidates, target) {
+  const len = candidates.length;
+  const result = [];
+
+  function solve(sum, index, output) {
+    if (sum === target) {
+      result.push([...output]);
+      return;
+    }
+    for (let i = 0; i < len; i++) {
+      sum += candidates[i];
+      if (sum <= target) {
+        output.push(candidates[i]);
+        solve(sum, index + 1, output);
+        sum -= candidates[i];
+        output.pop();
+      }
+    }
+  }
+
+  solve(0, 0, []);
+  return result;
+};
+
+console.log(combinationSum([2, 3, 6, 7], 7));
