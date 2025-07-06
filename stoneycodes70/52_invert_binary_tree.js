7170d7fa2d2792550d78f5e1b8a2d697481fa119
nischal dutt <nischaldutt01@gmail.com>
2025-07-07 02:19:39 +0530

    invert a binary tree

---
+++ stoneycodes70/52_invert_binary_tree.js
@@ -0,0 +1,9 @@
+var invertTree = function (root) {
+  if (!root) return null;
+  const temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+  invertTree(root.left);
+  invertTree(root.right);
+  return root;
+};
