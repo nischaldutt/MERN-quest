3d64355bb219e3103900d10ac6bd3b0a09f0105d
nischal dutt <nischaldutt01@gmail.com>
2025-09-05 12:31:43 +0530

    leetcode lca of bst

--- stoneycodes70/54_lowest_common_ancestor_of_bst.js
+++ stoneycodes70/54_lowest_common_ancestor_of_bst.js
@@ -1,9 +1,8 @@
 var lowestCommonAncestor = function (root, p, q) {
-  if (root === null) return null;
-  if (root.val >= p.val && root.val < q.val) return root;
+  if (!root) return null;
+  if (root.val < p.val && root.val < q.val)
+    return lowestCommonAncestor(root.right, p, q);
   else if (root.val > p.val && root.val > q.val)
     return lowestCommonAncestor(root.left, p, q);
-  else if (root.val < p.val && root.val < q.val)
-    return lowestCommonAncestor(root.right, p, q);
-  else return root;
+  return root;
 };
